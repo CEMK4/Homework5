4b8c1d79704acd75cd6c9df1dcf67930e7f6aa50
cemk4zxc <ceme4k4@mail.ru>
2022-05-22 16:17:09 +0300

    Реализовал необходимые методы.

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -10,6 +10,35 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            string sentence;
+            Console.WriteLine("Введите предложение:");
+            sentence = Console.ReadLine();
+            PrintWords(GetWords(sentence));
+
+            Console.WriteLine($"\nИсходное предложение в обротном порядке:\n{ReverseWords(sentence)}");
+        }
+
+        static string[] GetWords(string sentence)
+        {
+            return sentence.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static void PrintWords(string[] words)
+        {
+            Console.WriteLine("\nВсе слова предложения:");
+            foreach (string word in words)
+                Console.WriteLine(word);
+        }
+
+        static string ReverseWords(string sentence)
+        {
+            string[] words = GetWords(sentence);
+            string reverseSentence = "";
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                reverseSentence += words[i] + " ";
+            }
+            return reverseSentence;
         }
     }
 }
